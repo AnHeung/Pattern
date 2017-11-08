9a5959badc8e2b8d1e566ff3d2b2dbb4097cf0a8
DESKTOP-QLK1LVI\Kuma <gskebin4@gmail.com>
2017-11-08 14:31:41 +0900

    왜안되징

--- src/FactoryMethodPattern/ShapeTestMain.java
+++ src/FactoryMethodPattern/ShapeTestMain.java
@@ -5,10 +5,10 @@ public class ShapeTestMain {
 	
 	public static void main(String[] args) {
 		
+		//주석
 		Shape circle = new ShapeFactory().getShape("Circle");
 		circle.draw();
 		Shape square = new ShapeFactory().getShape("square");
 		square.draw();
-		
 	}
 }
